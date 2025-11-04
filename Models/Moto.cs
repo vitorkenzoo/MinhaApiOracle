f573f07d62acb4e5f0924ac0c5dd996b9a0e2bef
vitorkenzoo <vitormizumoto@hotmail.com>
2025-11-03 23:06:19 -0300

    moto

--- Models/Moto.cs
+++ Models/Moto.cs
@@ -1,11 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // Adicionado para [DatabaseGenerated]
 
 namespace MinhaApiOracle.Models
 {
     public class Moto
     {
         [Key]
+        // Esta é a linha que corrige o erro "IDENTITY_INSERT"
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdMoto { get; set; }
+        
         public string Modelo { get; set; } = null!;
         public string Placa { get; set; } = null!;
         public int Ano { get; set; }
